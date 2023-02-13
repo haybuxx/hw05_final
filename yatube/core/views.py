0fccb9da714c18e08159aff5b0b60c5a3ce96b65
Rinat Khaibullin <rinatkhaibullin@MacBook-Pro-Rinat.local>
2023-02-13 20:38:09 +0900

    6 rewiev

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from http import HTTPStatus
 
+
 def page_not_found(request, exception):
     return render(request, 'core/404.html', {'path': request.path},
                   status=HTTPStatus.NOT_FOUND)
